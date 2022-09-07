68cc20deb4aa54edbcc32032d69bfcc1b1d4c8ed
KevinCabrera25 <zap473@amerike.edu.mx>
2022-09-07 18:41:03 -0500

    Try on changing lights OnTriggerEnter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     // Variable to control the velocity of the movement
     private float velocity = 5f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,31 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(new Vector3(velocity, 0f, 0f) * Time.deltaTime);
         }
-
+        // Movement in -x direction
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector3(-velocity, 0f, 0f) * Time.deltaTime);
         }
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject[] cube;
+        cube = GameObject.FindGameObjectsWithTag("Cubes"); 
+        if(cube != null)
+        {
+            Debug.Log("Cube has been found");
+
+            /*
+            Light light = GameObject.FindObjectOfType<Light>();
+            if(light != null)
+            {
+                light.color = Color.red; 
+            }
+            */
+                   
+        }
+    }
+
 }

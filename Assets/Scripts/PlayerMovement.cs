2962b417576800c01898a27c48dbaf8da80e55d6
KevinCabrera25 <zap473@amerike.edu.mx>
2022-09-07 21:00:34 -0500

    Final Version of Tags and References

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     // Variable to control the velocity of the movement
     private float velocity = 5f;
-    
 
     // Start is called before the first frame update
     void Start()
@@ -32,21 +31,64 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        GameObject[] cube;
-        cube = GameObject.FindGameObjectsWithTag("Cubes"); 
-        if(cube != null)
-        {
-            Debug.Log("Cube has been found");
+        // ******** CUBES ********//
 
-            /*
+        // Variable to assign the GO
+        GameObject[] cube;
+        // Find the GO with the Tags Cubes
+        cube = GameObject.FindGameObjectsWithTag("Cubes");
+
+        // If the tag is distinct from null then we enter to the changing light condition
+        if (cube != null && other.gameObject.tag == "Cubes")
+        {
+            // Message in the console to know we enter in the event
+            Debug.Log("Cube has been found");
+            
+            // Find the Light GO
             Light light = GameObject.FindObjectOfType<Light>();
+            // If the GO light is found 
             if(light != null)
             {
+                // Then the color is changed to red
                 light.color = Color.red; 
-            }
-            */
-                   
+            }                               
         }
+
+        // ******** SPHERES ********//
+
+        // Variable to assign the GO
+        GameObject[] sphere;
+
+        // Find the GO with the Tags spheres
+        sphere = GameObject.FindGameObjectsWithTag("Spheres");
+
+        // If the tag is distinct from null then we enter to the changing light condition
+        if (sphere != null && other.gameObject.tag == "Spheres")
+        {
+            // Message in the console to know we enter in the event
+            Debug.Log("Sphere has been found");
+
+            // Find the Light GO
+            Light light = GameObject.FindObjectOfType<Light>();
+            // If the GO light is found 
+            if (light != null)
+            {
+                // Then the color is changed to red
+                light.color = Color.blue;
+            }
+        }
+        
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        // Find the Light GO
+        Light light = GameObject.FindObjectOfType<Light>();
+        // If the GO light is found 
+        if (light != null)
+        {
+            // Then the color is changed to white
+            light.color = Color.white;
+        }
+    }
 }
